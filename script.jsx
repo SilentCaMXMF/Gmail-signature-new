c1ad122f9927ad38a13ef2abed219ba010cb75cf
SilentCaMXMF <pedroocalado@gmail.com>
2025-08-31 22:47:07 +0100

    added click to call

--- script.jsx
+++ script.jsx
@@ -1,35 +1,31 @@
-<table border="0" cellspacing="0" cellpading="0" style="white-space:nowrap;width:440px!important;">
-    <tr>
-        <td style="white-space:nowrap;vertical-align:middle;width:113px">
-            <a href="https://silentcamxmf.github.io/" target="_blank"><img src="https://silentcamxmf.github.io/img/meCartoonDrumsResize.jpg" alt="image host"/></a>
-                </td>
-        <td
-            style="white-space:nowrap;font-family:Helvetica, Arial;padding-left: 10px;font-size: 14px;vertical-align:middle;white-space:nowrap">
-            <h1 style="white-space:nowrap;font-size:18px; color:#0e79ba;line-height: 1;margin: 0 0 8px 0">Pedro Farto Calado
-            </h1>
-            <h4
-                style="white-space:nowrap;font-size:14px; color:#333;line-height: 1;margin: 0 0 8px 0;font-weight: 400;">
-                Musica Tecnologia Educação 
-            </h4>
-            <h6 style="white-space:nowrap;font-size:14px; color:#999;line-height: 1;margin: 0;font-weight: 400;"><a
-                    href="mailto:pedroocalado@gmail.com"
-                    style="white-space:nowrap;text-decoration: none; color: #999">pedroocalado@gmail.com</a>{/* | <span
-                    style="white-space:nowrap;text-decoration:none!important;color: #999!important">(561)
-                    420-9316</span> */}
-            </h6>
-            <div style="white-space:nowrap;margin-top: 8px">
-                <a style="white-space:nowrap;display:inline-block;" href="https://github.com/SilentCaMXMF"
-                    target="_blank">
-                    <img style="white-space:nowrap;width: 32px;height:32px;"
-                        src="https://github.com/SilentCaMXMF/Gmail-signature-new/blob/main/img/GitHub-Mark-32px.png?raw=true" />
-                </a>
-                <a style="white-space:nowrap;display:inline-block;border-right: 1px solid #f1f1f1;padding-right: 5px"
-                    href="https://www.linkedin.com/in/pedro-calado-33113926/" target="_blank">
-                    <img style="white-space:nowrap;width: 32px;height:32px;"
-                        src="https://github.com/SilentCaMXMF/Gmail-signature-new/blob/main/img/linkedin_circle_color-32.png?raw=true" width="28" height="28" alt="LinkedIn" />
-        
-                </a>
-            </div>
-        </td>
-    </tr>
+<table border="0" cellspacing="0" cellpadding="0" style="width:440px!important;">
+  <tr>
+    <td style="vertical-align:middle;width:113px;">
+      <a href="https://silentcamxmf.github.io/" target="_blank">
+        <img src="https://silentcamxmf.github.io/img/meCartoonDrumsResize.jpg" alt="Pedro Farto Calado Website" style="border:none;display:block;max-width:113px;"/>
+      </a>
+    </td>
+    <td style="font-family:Helvetica, Arial, sans-serif;padding-left:10px;font-size:14px;vertical-align:middle;">
+      <h1 style="font-size:18px; color:#0e79ba;line-height:1;margin:0 0 8px 0;">
+        Pedro Farto Calado
+      </h1>
+      <h4 style="font-size:14px; color:#444;line-height:1;margin:0 0 8px 0;font-weight:400;">
+        Música • Tecnologia • Educação
+      </h4>
+      <h6 style="font-size:14px; color:#777;line-height:1;margin:0;font-weight:400;">
+        <a href="mailto:pedroocalado@gmail.com" style="color:#777;text-decoration:none;">pedroocalado@gmail.com</a>
+      </h6>
+      <h6 style="font-size:14px; color:#777;line-height:1;margin:0;font-weight:400;">
+        <a href="tel:+351918331724" style="color:#777;text-decoration:none;">📞 +351 918 331 724</a>
+      </h6>
+      <div style="margin-top:8px;">
+        <a href="https://github.com/SilentCaMXMF" target="_blank" style="display:inline-block;margin-right:5px;">
+          <img src="https://github.com/SilentCaMXMF/Gmail-signature-new/blob/main/img/GitHub-Mark-32px.png?raw=true" alt="GitHub" style="width:32px;height:32px;border:none;"/>
+        </a>
+        <a href="https://www.linkedin.com/in/pedro-calado-33113926/" target="_blank" style="display:inline-block;padding-right:5px;border-right:1px solid #ddd;">
+          <img src="https://github.com/SilentCaMXMF/Gmail-signature-new/blob/main/img/linkedin_circle_color-32.png?raw=true" alt="LinkedIn" style="width:32px;height:32px;border:none;"/>
+        </a>
+      </div>
+    </td>
+  </tr>
 </table>
